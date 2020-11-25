9842179943631aaf0552d98c9df172a0d90b9e60
Joshua Patton <70239458+pattonjosh@users.noreply.github.com>
2020-11-25 12:56:30 -0800

    Added new project
    Added new project to src_old

---
+++ src/src/edu/jeffstate/MapMaker.java
@@ -0,0 +1,52 @@
+package edu.jeffstate;
+
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+public class MapMaker extends Application{
+    public MapMaker() {
+        super();
+    } //MapMaker()
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        MapMaker mapMaker = new MapMaker();
+        launch(args);
+    } //main
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MapMakerGUI.fxml"));
+            
+            var scene = new Scene(root, 600, 400);       
+            
+            stage.setTitle("MapMaker");
+            stage.setScene(scene);
+            stage.show();
+            
+        } catch (Exception ex){
+            ex.printStackTrace();
+            
+        }
+        
+    } //start
+    
+    @Override
+    public void stop() {
+        System.out.println("Exiting via MapMaker.stop method");
+        
+        System.exit(0);
+    } //stop
+
+} //MapMaker
